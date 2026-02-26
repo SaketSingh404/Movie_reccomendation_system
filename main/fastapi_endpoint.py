c823e5f19a1681f9e4295954d3d26a69f94b3d5f
Saket <saketsinghofficial404@gmail.com>
2026-02-27 01:46:57 +0530

    Endpoint output formatting fixed

--- main/fastapi_endpoint.py
+++ main/fastapi_endpoint.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
 import pickle
 import pandas as pd
-
+from fastapi.middleware.cors import CORSMiddleware
 
 similarity_matrix = pickle.load(open("main/similarity.pkl","rb"))
 new_df = pd.read_csv("main/api_dataframe.csv")
 
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Allows all origins
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def hello():
     return {'message':'Hello world'}
@@ -16,7 +21,7 @@ def hello():
 @app.get('/recommend/{index}')
 def recommend(index:int):
     distances = similarity_matrix[index]
-    movies_list = sorted(list(enumerate(distances)),reverse=True,key = lambda x:x[1])[1:11]
+    movies_list = sorted(list(enumerate(distances)),reverse=True,key = lambda x:x[1])[1:16]
     recommended_index = [i[0] for i in movies_list]
     recommended_df = new_df.iloc[recommended_index]
-    return recommended_df.set_index("title")["poster_path"].to_dict()
+    return recommended_df.reset_index()[['title','poster_path']].to_dict(orient='records')
